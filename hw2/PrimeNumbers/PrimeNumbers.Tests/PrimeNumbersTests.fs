26adcf3936256572b5bd6b3ca34e5680910367c8
YakovlevAl <sanya89_muravlenko@mail.ru>
2024-03-15 05:00:37 +0300

    add tests

--- hw2/PrimeNumbers/PrimeNumbers.Tests/PrimeNumbersTests.fs
+++ hw2/PrimeNumbers/PrimeNumbers.Tests/PrimeNumbersTests.fs
@@ -6,5 +6,5 @@ open PrimeNumbersGenerator
 
 [<Test>]
 let TestFirst15PrimeNumbersAreEqual () =
-    let expected = seq {2;3;5;7;11;13;17;23;29;31;37;41;43;47;53}
+    let expected = seq {2;3;5;7;11;13;17;19;23;29;31;37;41;43;47}
     PrimeGenerator() |> Seq.take 15 |> should equal expected
