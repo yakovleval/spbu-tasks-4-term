501eae905164b26e7e55e9d62bc9581f243999ca
YakovlevAl <sanya89_muravlenko@mail.ru>
2024-03-15 10:42:22 +0300

    Even Numbers

---
+++ hw2/EvenNumbers/EvenNumbers/Library.fs
@@ -0,0 +1,16 @@
+﻿namespace EvenNumbers
+
+module EvenNumbers =
+    let byMap = 
+        List.map (fun x -> x % 2) >> List.filter (fun x -> x = 0) >> List.length
+
+    let byFilter =
+        List.filter (fun x -> x % 2 = 0) >> List.length
+
+    let folder state x =
+        if x % 2 = 0 then
+            state + 1
+        else
+            state
+    let byFold =
+        List.fold folder 0
