5bf85d39d7e9afdab08cbce9506b189db7d8caea
YakovlevAl <sanya89_muravlenko@mail.ru>
2024-03-15 04:43:27 +0300

    prime numbers generator

---
+++ hw2/PrimeNumbers/PrimeNumbers/Program.fs
@@ -0,0 +1,11 @@
+﻿module PrimeNumbersGenerator
+let IsPrime number =
+    match number with
+        | _ when number <= 1 -> false
+        | _ -> 
+            let root = number |> float |> sqrt |> int
+            seq{2 .. root}
+            |> Seq.filter(fun divisor -> number % divisor = 0)
+            |> Seq.length = 0
+let PrimeGenerator() = Seq.initInfinite(fun n -> n) |> Seq.filter(IsPrime)
+printfn "%A" (PrimeGenerator() |> Seq.take 15)
